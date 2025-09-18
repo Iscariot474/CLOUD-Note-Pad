1addbc5ec78610157ef36a04997405b5e3762a8e
Judas <123628394+Iscariot474@users.noreply.github.com>
2025-09-18 17:22:58 +0100

    Create frontend.js

---
+++ frontend.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", maxWidth: 700, margin: "auto" }}>
+      <h1>Cloud Notes</h1>
+      {/* Add Login, NoteEditor, GithubEmbed components here */}
+      <p>Cloud Note.</p>
+    </div>
+  );
+}
+
+export default App;
