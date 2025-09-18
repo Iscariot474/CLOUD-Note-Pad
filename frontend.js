d4a0846e47651aab9ceb7a4c9fcbbdbceb9bfc86
Judas <123628394+Iscariot474@users.noreply.github.com>
2025-09-18 17:23:36 +0100

    frontend file

--- frontend.js
+++ frontend.js
@@ -5,7 +5,7 @@ function App() {
     <div style={{ padding: "2rem", maxWidth: 700, margin: "auto" }}>
       <h1>Cloud Notes</h1>
       {/* Add Login, NoteEditor, GithubEmbed components here */}
-      <p>Cloud Note.</p>
+      <p>Cloud Note USE ME.</p>
     </div>
   );
 }
